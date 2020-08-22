109262d28a163a7e4416931836474d4e83a39f8a
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-22 17:40:18 +0530

    getConnectionConfig return type improved

--- src/Providers/DatabaseConfigProvider.ts
+++ src/Providers/DatabaseConfigProvider.ts
@@ -1,18 +1,19 @@
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { createConnection } from 'typeorm';
 
 export class DatabaseConfigProvider {
   // Returns Configuration for TypeORM database Connection
-  public static getConnectionConfig = (): PostgresConnectionOptions => ({
-    type: 'postgres',
-    host: process.env.TYPEORM_HOST!,
-    port: +process.env.TYPEORM_PORT!,
-    username: process.env.TYPEORM_USERNAME!,
-    password: process.env.TYPEORM_PASSWORD!,
-    database: process.env.TYPEORM_DATABASE!,
-    entities: ['Entity/*.ts'],
-    migrations: ['Migration/*.ts'],
-    cli: {
-      migrationsDir: 'Migration',
-    },
-  });
+  public static getConnectionConfig = () =>
+    ({
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST!,
+      port: +process.env.TYPEORM_PORT!,
+      username: process.env.TYPEORM_USERNAME!,
+      password: process.env.TYPEORM_PASSWORD!,
+      database: process.env.TYPEORM_DATABASE!,
+      entities: ['Entity/*.ts'],
+      migrations: ['Migration/*.ts'],
+      cli: {
+        migrationsDir: 'Migration',
+      },
+    } as Parameters<typeof createConnection>[0]);
 }
