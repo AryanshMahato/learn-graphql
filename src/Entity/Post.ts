14179b077b4ef346c27bfb155a30fdaed9d22491
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-22 17:47:13 +0530

    Styling fixed!

--- src/Entity/Post.ts
+++ src/Entity/Post.ts
@@ -10,15 +10,15 @@ import {
 export class Post {
   @PrimaryGeneratedColumn('uuid')
   id: string;
-  
+
   @Column()
   @CreateDateColumn({ name: 'created_at' })
   createdAt = new Date();
-  
+
   @Column()
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt = new Date();
-  
+
   @Column()
   title: string;
 }
