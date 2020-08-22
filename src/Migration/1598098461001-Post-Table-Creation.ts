3fb505703f72597a21d968def0835e3ebe7e5dfe
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-22 17:46:10 +0530

    Initial Migration Added

--- src/Migration/1598098461001-Post-Table-Creation.ts
+++ src/Migration/1598098461001-Post-Table-Creation.ts
@@ -4,11 +4,12 @@ export class PostTableCreation1598098461001 implements MigrationInterface {
     name = 'PostTableCreation1598098461001';
     
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "title" character varying NOT NULL, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(
+          `CREATE TABLE "posts" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "title" character varying NOT NULL, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`,
+        );
     }
     
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP TABLE "posts"`);
     }
-    
 }
